bb22492b4800c401d77270bf9289f9d5526ebb1d
thecode00 <thecode0087@gmail.com>
2023-02-15 06:02:55 +0900

    Add reset-css and move file

--- socket.js
+++ socket.js
@@ -1,6 +1,11 @@
-import io from "socket.io-client";
+const ws = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@trade");
 
-const socket = io("wss://stream.binance.com:9443/ws/btcusdt@depth10@100ms");
-socket.on("connect", (msg) => {
-  console.log(msg);
-});
+// 거래가격을 불러오고 index에 가격을 표시하는 작업
+ws.onmessage = (msg) => {
+	const json = JSON.parse(msg.data);
+	console.log(json.p);
+	let text = document.createElement("p");
+	text.innerText = json.p;
+	console.log(text);
+	document.body.appendChild(text);
+};
